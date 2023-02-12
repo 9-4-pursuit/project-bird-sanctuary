5bd0a141c70502291f672fac0a2b80559f57b9c0
USERNAME <EMAIL>
2023-02-12 12:25:19 -0500

    basic layout of bird cards

---
+++ src/components/BirdCards.js
@@ -0,0 +1,29 @@
+
+function BirdCards (props) {
+    const data = props.birdData
+    console.log(data)
+  
+    return (
+
+        <div className="card">
+            
+            {data.map((bird) => {
+          return (
+           
+            <div key={bird.id} className="birds">
+              <li>
+                <p>{bird.name}</p>
+                <p>Price: ${bird.amount}</p>
+                <img className = "img" src={bird.img} alt="birdimage" /><br></br>
+              <button>Adopt Me</button>
+              </li>
+              </div>
+            
+          );
+        })}
+           
+        </div>
+)
+}
+export default BirdCards
+
