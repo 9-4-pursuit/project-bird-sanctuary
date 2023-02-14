7f03cdf56de7339b1f646ec0a198fa41c99a4e96
ninajavier <ninajavier13@gmail.com>
2023-02-13 20:58:02 -0500

    Created a functioning checkout component

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,10 +1,36 @@
 import React from 'react';
 
 function Checkout(props) {
+    
+    function handleSubmit() {
+        alert("You have adopted birds. Thank you!")
+    }
     return (
-        <div>
-            
-        </div>
+        <form onSubmit={handleSubmit} class="Checkout" >
+              <h3>Checkout</h3>
+              <label htmlFor="first-name">First Name</label>
+              <input
+                type="text"
+                id="first-name"
+              />
+              <label htmlFor="last-name">Last Name</label>
+              <input
+                type="text"
+                id="last-name"
+              />
+            <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+              />
+              <label htmlFor="zip">Zip Code</label>
+              <input
+                type="text"
+                pattern="[0-9]{5}"
+                id="zip"
+              />
+              <input type="submit" />
+            </form>
     );
 }
 
