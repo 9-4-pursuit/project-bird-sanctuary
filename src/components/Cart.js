1392aa198b91893a94964a371bfd164405232fde
USERNAME <EMAIL>
2023-02-14 17:49:53 -0500

    small css update

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,10 +6,10 @@
 function Cart ({adoptBird, cartTotal}) {
     return (
         <div className="cart">
-            <h1><center>Cart</center></h1>
-            <p><center><i>Discount 0%</i></center></p>
-            <h4><center><strong>Total: $0</strong></center></h4>
-            <p><center>Your donations qualify you for the following items</center></p>
+            <h1>Cart</h1>
+            <p><i>Discount 0%</i></p>
+            <h4><strong>Total: $0</strong></h4>
+            <p>Your donations qualify you for the following items</p>
 
         </div>
     )
