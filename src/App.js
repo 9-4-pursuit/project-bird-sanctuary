bfb9147733d7c460dbe2bc2ede3939a2e6da4986
Wilsantos1975 <Faustosantos@pursuit.org>
2023-02-15 00:54:11 -0500

    added bonus items

--- src/App.js
+++ src/App.js
@@ -4,36 +4,90 @@ import BirdCard from "./Components/BirdCard";
 import birdData from "./data/birds";
 import Checkout from "./Components/Checkout";
 import "./App.css";
-// import bonusItems from "./data/bonusItems";
+import bonusItems from "./data/bonusItems";
 
 function App() {
   const [adoptBirdTotal, setAdoptBirdTotal] = useState(0);
   const [cart, setCart] = useState([]);
-  const [discount, setDiscount] = useState(0);
+  const [bonus, setBonus] = useState([]);
 
   function handleAdopt(bird) {
     if (cart.includes(bird)) {
-      bird = {...bird, id: parseInt(`${bird.id}${cart.length}`)};
+      bird = { ...bird, id: parseInt(`${bird.id}${cart.length}`) }; //this checks if the bird that comes from the birdcard is the same one inside the array and is it is add a new number ti the id making it exclusive
     }
     setCart([...cart, bird]);
     setAdoptBirdTotal(adoptBirdTotal + bird.amount);
+
+    // if (
+    //   adoptBirdTotal + bird.amount >= 100 &&
+    //   adoptBirdTotal + bird.amount <= 300
+    // ) {
+    //   setBonus([bonusItems[0]]);
+    //   console.log(1)
+    // } else if (
+    //   adoptBirdTotal + bird.amount > 300 &&
+    //   adoptBirdTotal + bird.amount <= 500
+    // ) {
+    //   setBonus([bonusItems[0], bonusItems[1]]);
+    //   console.log(2)
+    // } else if (
+    //   adoptBirdTotal + bird.amount > 500 &&
+    //   adoptBirdTotal + bird.amount < 1000
+    // ) {
+    //   setBonus([bonusItems[0], bonusItems[1], bonusItems[2]]);
+    //   console.log(3)
+    // } else {
+    //   setBonus(bonusItems);
+    // }
+        handleBonus(adoptBirdTotal , bird)
   }
 
   function deleteBird(bird) {
     const newCart = cart.filter((birdItem) => bird.id !== birdItem.id);
     setCart(newCart);
     setAdoptBirdTotal(adoptBirdTotal - bird.amount);
+    handleBonus(adoptBirdTotal , bird)
   }
+
+  function handleBonus(adoptBirdTotal, bird) {
+console.log(adoptBirdTotal,bird)
+
+    if (
+      adoptBirdTotal + bird.amount >= 100 &&
+      adoptBirdTotal + bird.amount <= 300
+    ) {
+      setBonus([bonusItems[0]]);
+    } else if (
+      adoptBirdTotal + bird.amount > 300 &&
+      adoptBirdTotal + bird.amount <= 500
+    ) {
+      setBonus([bonusItems[0], bonusItems[1]]);
+    } else if (
+      adoptBirdTotal + bird.amount > 500 &&
+      adoptBirdTotal + bird.amount < 1000
+    ) {
+      setBonus([bonusItems[0], bonusItems[1], bonusItems[2]]);
+    } else {
+      setBonus(bonusItems);
+    }
+  }
+
+  // function handleBonus () {
+  //   if (cart.length 1) {
+
+  //   }
+  // }
 
   return (
     <div className="app">
-      <Cart
+      <Cart key={"cartKey"}
         adoptBirdTotal={adoptBirdTotal}
         cart={cart}
         setAdoptBirdTotal={setAdoptBirdTotal}
         deleteBird={deleteBird}
-        discount={discount}
-        setDiscount={setDiscount}
+        bonusItems={bonusItems}
+        bonus={bonus}
+        handleBonus={handleBonus}
       />
       <BirdCard birdData={birdData} handleAdopt={handleAdopt} />
       <Checkout />
--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,27 +1,30 @@
-
-
-function Cart({ cart, adoptBirdTotal,deleteBird, discount, setDiscount}) {
-
-
+function Cart({ cart, adoptBirdTotal, deleteBird, handleBonus, bonus }) {
   return (
     <div className="cart">
       <h1>Cart</h1>
-      <p>Discount: %{discount}</p>
+      <p>Discount: {cart.length >= 3 ? 10 : 0} %</p>
       <h4>
         <strong>Total:$ {adoptBirdTotal}</strong>
       </h4>
-      {cart.map((bird) => {
-          adoptBirdTotal = bird.amount
+      <ol>
+        {cart.map((bird) => {
+          adoptBirdTotal = bird.amount;
           return (
-              <ol key={bird.id}>
-            <li>
+            <li key={bird.id}>
               {bird.name} ${bird.amount}.00
+              <button onClick={() => deleteBird(bird)}>delete</button>
             </li>
-            <button onClick={() => deleteBird(bird)}>delete</button>
-          </ol>
-        );
-      })}
+          );
+        })}
+      </ol>
       <p>Your donations has qualified you for the following items</p>
+      <ul>
+        {
+        bonus &&
+          bonus.map((item,i) => {
+            return <li key={`bonus${i}`}>{item}</li>;
+          })}
+      </ul>
     </div>
   );
 }
