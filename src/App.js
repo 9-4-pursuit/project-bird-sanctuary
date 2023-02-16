58e84b894b74bad918890e132076b03bfc363a73
Wilsantos1975 <Faustosantos@pursuit.org>
2023-02-15 21:27:59 -0500

    deleting item bonus troubleshooting complete, had to use useEffect with help oof instructors

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import bonusItems from "./data/bonusItems";
 function App() {
   const [adoptBirdTotal, setAdoptBirdTotal] = useState(0);
   const [cart, setCart] = useState([]);
-  const [bonus, setBonus] = useState([]);
+ 
+
+
 
   function handleAdopt(bird) {
     if (cart.includes(bird)) {
@@ -17,77 +19,33 @@ function App() {
     }
     setCart([...cart, bird]);
     setAdoptBirdTotal(adoptBirdTotal + bird.amount);
-
-    // if (
-    //   adoptBirdTotal + bird.amount >= 100 &&
-    //   adoptBirdTotal + bird.amount <= 300
-    // ) {
-    //   setBonus([bonusItems[0]]);
-    //   console.log(1)
-    // } else if (
-    //   adoptBirdTotal + bird.amount > 300 &&
-    //   adoptBirdTotal + bird.amount <= 500
-    // ) {
-    //   setBonus([bonusItems[0], bonusItems[1]]);
-    //   console.log(2)
-    // } else if (
-    //   adoptBirdTotal + bird.amount > 500 &&
-    //   adoptBirdTotal + bird.amount < 1000
-    // ) {
-    //   setBonus([bonusItems[0], bonusItems[1], bonusItems[2]]);
-    //   console.log(3)
-    // } else {
-    //   setBonus(bonusItems);
-    // }
-        handleBonus(adoptBirdTotal , bird)
+    // handleBonus(adoptBirdTotal, bird);
   }
 
   function deleteBird(bird) {
     const newCart = cart.filter((birdItem) => bird.id !== birdItem.id);
     setCart(newCart);
     setAdoptBirdTotal(adoptBirdTotal - bird.amount);
-    handleBonus(adoptBirdTotal , bird)
+    // handleBonus(adoptBirdTotal, bird);
   }
 
-  function handleBonus(adoptBirdTotal, bird) {
-console.log(adoptBirdTotal,bird)
+  
+  
 
-    if (
-      adoptBirdTotal + bird.amount >= 100 &&
-      adoptBirdTotal + bird.amount <= 300
-    ) {
-      setBonus([bonusItems[0]]);
-    } else if (
-      adoptBirdTotal + bird.amount > 300 &&
-      adoptBirdTotal + bird.amount <= 500
-    ) {
-      setBonus([bonusItems[0], bonusItems[1]]);
-    } else if (
-      adoptBirdTotal + bird.amount > 500 &&
-      adoptBirdTotal + bird.amount < 1000
-    ) {
-      setBonus([bonusItems[0], bonusItems[1], bonusItems[2]]);
-    } else {
-      setBonus(bonusItems);
-    }
-  }
-
-  // function handleBonus () {
-  //   if (cart.length 1) {
-
-  //   }
-  // }
+ 
 
   return (
     <div className="app">
-      <Cart key={"cartKey"}
+      <Cart
+        key={"cartKey"}
         adoptBirdTotal={adoptBirdTotal}
         cart={cart}
         setAdoptBirdTotal={setAdoptBirdTotal}
         deleteBird={deleteBird}
         bonusItems={bonusItems}
-        bonus={bonus}
-        handleBonus={handleBonus}
+        // bonus={bonus}
+        // handleBonus={handleBonus}
+        
       />
       <BirdCard birdData={birdData} handleAdopt={handleAdopt} />
       <Checkout />
--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,6 +1,30 @@
-function Cart({ cart, adoptBirdTotal, deleteBird, handleBonus, bonus }) {
+import { useState, useEffect } from "react";
+
+function Cart({ cart, adoptBirdTotal, deleteBird, bonusItems }) {
+  const [bonus, setBonus] = useState([]);
+
+  useEffect(() => {
+    handleBonus(adoptBirdTotal);
+  }, [cart]);
+
+  function handleBonus(adoptBirdTotal) {
+
+if (adoptBirdTotal === 0) {
+setBonus([])
+}    else if (adoptBirdTotal >= 100 && adoptBirdTotal <= 300) {
+      setBonus([bonusItems[0]]);
+    } else if (adoptBirdTotal > 300 && adoptBirdTotal <= 500) {
+      setBonus([bonusItems[0], bonusItems[1]]);
+    } else if (adoptBirdTotal > 500 && adoptBirdTotal < 1000) {
+      setBonus([bonusItems[0], bonusItems[1], bonusItems[2]]);
+    } 
+    else {
+      setBonus(bonusItems);
+    }
+  }
+
   return (
-    <div className="cart">
+    <div className="Cart">
       <h1>Cart</h1>
       <p>Discount: {cart.length >= 3 ? 10 : 0} %</p>
       <h4>
@@ -19,9 +43,8 @@ function Cart({ cart, adoptBirdTotal, deleteBird, handleBonus, bonus }) {
       </ol>
       <p>Your donations has qualified you for the following items</p>
       <ul>
-        {
-        bonus &&
-          bonus.map((item,i) => {
+        {bonus &&
+          bonus.map((item, i) => {
             return <li key={`bonus${i}`}>{item}</li>;
           })}
       </ul>
