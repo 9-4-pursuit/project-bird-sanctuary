65b07514114475d310c5dcca949e6d77b2999df3
JoseC620 <Jcepeda6201@gmail.com>
2023-02-15 22:13:33 -0500

    couldnt get last test to pass

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,90 @@
-import { useState } from "react";
-import BirdCards from "./BirdCards";
-import { v1 as generateUniqueID } from "uuid";
 
 
-function Cart(props){
 
-    const [total, setTotal] = useState()
+function Cart({ bonus, prevBird, total, remove}){
+
+    let discount = ""
+    
+    let theBonus = []
+
+    let sentence = ""
+
+    let idArr = []
+
+    function addName() {
+    for(let i = 0; i < idArr.length; i++){
+        for(let j = 0; j < idArr.length; j++){
+        if(!idArr.length){
+            idArr.push(prevBird)
+            console.log(idArr)
+            console.log(prevBird)
+        } 
+    }
+}
+    }
+   
+    function determineDiscount(){
+        if(prevBird.length > 2){
+            discount = "10%"
+        } else if(prevBird.length <= 2){
+        discount = "0%"
+    }
+    }
+
+    function determineBonus() {
+        if(total >= 100){
+            sentence = "Your donation has qualified you for the following items. "
+            theBonus = bonus[0]
+            if (total >= 300){
+            theBonus += ", "
+            theBonus += bonus[1]
+        }   if (total >= 500){
+            theBonus += ", "
+            theBonus += bonus[2]
+        }   if (total >= 1000){
+            theBonus += ", "
+            theBonus += bonus[3]
+        }
+    }
+    }
+
+    function addLi() {
+        if(total >= 100){
+            return(
+                <div>
+                <p>{sentence}</p>
+                <li>{theBonus}</li>
+                </div>
+            )
+            
+        }
+    }
+
+    determineBonus()
+    determineDiscount()
+    addName()
+
+    let count = 0
 
     return (
-        <div>
-           
+        
+        <div className="Cart">
+            <h3>Cart</h3>
+            <p>Discount: {discount}</p>
+            <h4>Total: ${total}</h4>
+                {prevBird.map((bird) => {
+                    count++
+                    return(
+                    <div key={bird.id}>
+                        <li>
+                            {count}. {bird.name}: ${bird.amount}
+                            <button onClick={() => remove(bird)}>Remove.</button>
+                        </li>
+                     </div>
+                    )
+                })}
+                <br></br>
+                {addLi()}
         </div>
     )
 }
--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,8 +1,27 @@
-import { v1 as generateUniqueID } from "uuid";
 
 
-function Checkout(){
-    
+
+function Checkout({ submit }){
+
+
+    return (
+        <div className="Checkout"> 
+            <h3>Checkout</h3>
+            <form>
+                <label htmlFor="firstName">First Name</label>
+                <input id="firstName" type="text" />
+                <label htmlFor="lastName">Last Name</label>
+                <input id="lastName" type="text"/>
+                <label htmlFor="email">Email</label>
+                <input id="email" type="email"/>
+                <label htmlFor="zipCode">Zip Code</label>
+                <input id="zipCode" type="number"/>
+                <input type="submit" onClick={submit}/>
+            </form>
+        </div>
+    )
+
+
 }
 
 
